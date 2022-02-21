2ad3821220cfc4245e5e5227599c7fd28d9a2228
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-02-21 13:35:44 +0100

    add Layout component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>hello from footer</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/projects/bachelors.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const bachelors = () => {
+  return (
+    <div>
+      <h1>Bachelors Degree Project</h1>
+      <h2>Stronger Together? An Ensemble of CNNs for Deepfakes Detection</h2>
+    </div>
+  )
+}
+
+export default bachelors
